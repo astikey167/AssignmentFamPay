d91b587e0e41e2702009086eb5b942af3370bb2e
astikey167 <astikey070@gmail.com>
2023-01-23 10:04:25 +0530

    Update fetchvideo.py

--- script/fetchvideo.py
+++ script/fetchvideo.py
@@ -1,159 +1,82 @@
-import threading
-import os,time
-import datetime
-from turtle import title
-from googleapiclient.discovery import build
+from flask import Flask, request, jsonify, redirect, url_for,render_template
+from flask_restful import Resource, Api
+import hvac,os,json
+import sys, jwt, yaml
 import mysql.connector
+from flask_cors import CORS
+from functools import wraps
+from dateutil import parser
+app = Flask(__name__)
+api = Api(app)
+CORS(app, resources={r'/*': {'origins': '*'}})
+with open("./script/config.json",'a') as f :     #loading variables from config.json
+    data=json.loads(f)
 
-WAIT_TIME=10
-QUERY="CRICKET"
-PAGES_MAX=5
-API_TOKN=""
-CLIENT_ID="553581124539-giiet56h3njespipm4j5tn05rvqp9g96.apps.googleusercontent.com"
-CLIENT_PWD=""
-MYSQL_HOST = "127.0.0.1"
-MYSQL_USER= "root"
-MYSQL_PASSWORD= "mariadb"
-MYSQL_DB = "start"
-MAXRESULTS=20
-#youtube=build('youtube','v3',developerKey=API_TOKN)
-dataBase = mysql.connector.connect(host =MYSQL_HOST,user =MYSQL_USER,passwd =MYSQL_PASSWORD,database=MYSQL_DB) 
-lock=threading.Lock()
+MYSQL_HOST =data["MYSQL_HOST"]
+MYSQL_USER= data["MYSQL_USER"]
+MYSQL_PASSWORD= data["MYSQL_PASSWORD"]
+MYSQL_DB = data["MYSQL_DB"]
 
-def heal(e):
-    if e=="MySQL Connection not available." :
-        dataBase = mysql.connector.connect(host =MYSQL_HOST,user =MYSQL_USER,passwd =MYSQL_PASSWORD,database=MYSQL_DB) 
-
-class DatabaseOp :
-    """
-    this class basically does the pushes the entries into the database
-    """
+class DBOps :
     def __init__(self):
-        try :
-            lock.acquire()
-            self.cursorObject=dataBase.cursor()
-            self.videos_record = """CREATE TABLE VIDEOS (
-                    TAG VARCHAR(100) NOT NULL,
-                    TITLE  VARCHAR(500) NOT NULL,
-                    DESCRIPTION VARCHAR(500),
-                    PUB_TIME VARCHAR(100),
-                    THUMB_URLS VARCHAR(200),
-                    CHANNELNAME VARCHAR(200)
-                    )"""
-            self.cursorObject.execute(self.videos_record)    #creating the table for the start
-            lock.release()
-        except Exception as e :
-            heal(e)
-            lock.release()
-            print("database already created")            #if the table is alrady created it will throw error                     
-
-    def put_data(self,tag,title,description,pub_time,urls,channeltitle) :
-        # function to push entries to the table
-        #print(tag,title,description,pub_time,urls,channeltitle)
-        try :
-            print("cursor created")
-            query="INSERT INTO VID2 (TAG,TITLE,DESCRIPTION,PUB_TIME,THUMB_URLS,CHANNELNAME) VALUES (%s,%s,%s,%s,%s,%s)"
-            val = (tag,title, description, pub_time, urls, channeltitle)
-            print("locking")
-            lock.acquire()
-            cursorObject = dataBase.cursor()
-            cursorObject.execute(query, val)
-            dataBase.commit()
-            lock.release()
-            print("releasing lock 1")
-        except Exception as e :
-            lock.release()
-            print("releasing lock 2")
-            heal(e)
-            print("error while inserting entry",tag,title,description,pub_time,urls,channeltitle)
-            print(e)
+        self.dataBase = mysql.connector.connect(host =MYSQL_HOST,user =MYSQL_USER,passwd =MYSQL_PASSWORD,database=MYSQL_DB)
+    
+    def get_video_title(self,title) :
+        query="SELECT * FROM VID2 where TITLE='{}'".format(title)
+        create_cursor = self.dataBase.cursor()
+        create_cursor.execute(query)
+        response=[]
+        for x in create_cursor:
+            print(x[0])
+            response.append({"tag":x[0],"title":x[1],"DESCRIPTION":x[2],"UploadTime":x[3],"ThumbnailUrl":x[4],"ChannelName":x[5]})
         
+        return response
 
-class DataOp :
-    """
-    This class basically take the fetched json data, traverses over the entries and pushes them parallely using threading
-    """
-    def __init__(self):
-        print("DataOp initiated")
-        pass
+    def get_video_description(self,description) :
+        query="SELECT * FROM VID2 where DESCRIPTION='{}'".format(description)
+        create_cursor = self.dataBase.cursor()
+        create_cursor.execute(query)
+        response=[]
+        for x in create_cursor:
+            response.append({"tag":x[0],"title":x[1],"DESCRIPTION":x[2],"UploadTime":x[3],"ThumbnailUrl":x[4],"ChannelName":x[5]})
+        
+        return response
 
-    def data_operation(self,response,DatabaseOp):
-        try :
-            no_threads= 10                              #size of thread pool
-            no_threads=min(no_threads,response["pageInfo"]["resultsPerPage"])   
-            i=0
-            threads=[]                                  #thread pool
-            for item in response["items"] :
-                tag=item["etag"]
-                pub_time=item["snippet"]["publishedAt"]
-                description=item["snippet"]["description"]
-                urls=item["snippet"]["thumbnails"]["high"]["url"]
-                title=item["snippet"]["title"]
-                channeltitle=item["snippet"]["channelTitle"]
-                i=i+1
-                print("starting thread")
-                t=threading.Thread(target=DatabaseOp.put_data,args=[tag,title,description,pub_time,urls,channeltitle])
-                t.start()
-                threads.append(t)
-                if i==no_threads :                     # when size of thread pool is full
-                    for thread in threads :
-                        thread.join()                  # waiting for all threads to execute in thread pool
-                    i=0
-                    threads=[]                         # hence emptying the thread pool
-            return 
-        except Exception as e :
-            heal(e)
-            print("expected error occured : ",e)
-            
-
-            
+    def get_all_data(self):
+        query="SELECT * FROM VID2"
+        create_cursor = self.dataBase.cursor()
+        create_cursor.execute(query)
+        response=[]
+        for x in create_cursor:
+            response.append({"tag":x[0],"title":x[1],"DESCRIPTION":x[2],"UploadTime":x[3],"ThumbnailUrl":x[4],"ChannelName":x[5]})
+        
+        response.sort(key=lambda val: parser.parse(val["UploadTime"]))
+        return response
 
 
+db_operations=DBOps()
 
-class SearchVideosYoutube :
-    """
-    This class basically uses the youtube api call to fetch the info for the query
-    """
-    def __init__(self):
-        self.youtube=build('youtube','v3',developerKey=API_TOKN)
-        self.db_op=DatabaseOp()
-        self.dataop=DataOp()
+class SearchVideo(Resource) :
+    def get(self):
+        data=request.headers
+        content_type = data.get('Content-Type')
+        if (content_type == 'application/json'):
+            try :
+                title=request.headers['title']
+                return db_operations.get_video_title(title)
+            except Exception as e:
+                description=request.headers['description']
+                return db_operations.get_video_description(description)
 
-    def search_videos_since(self,tm) :
-        request=self.youtube.search().list(part="id,snippet",type='video',q=QUERY,videoDuration='short',videoDefinition='high',maxResults=MAXRESULTS,publishedAfter=tm)
-        response = request.execute()
-        self.dataop.data_operation(response,self.db_op)
-        while(response.get("nextPageToken")) :
-            request=self.youtube.search().list(part="id,snippet",type='video',q=QUERY,videoDuration='short',videoDefinition='high',maxResults=MAXRESULTS,publishedAfter=tm,pageToken=response["nextPageToken"])
-            response = request.execute()
-            self.dataop.data_operation(response,self.db_op)
+class FetchVideo(Resource) :
+    def get(self):
+        data=request.headers
+        content_type = data.get('Content-Type')
+        if (content_type == 'application/json'):
+            return db_operations.get_all_data()
 
-    def search_videos(self) :
-        request=self.youtube.search().list(part="id,snippet",type='video',q=QUERY,videoDuration='short',videoDefinition='high',maxResults=MAXRESULTS)
-        response = request.execute()
-        #print(response)
-        self.dataop.data_operation(response,self.db_op)
-        for i in range(0,PAGES_MAX):
-            request=self.youtube.search().list(part="id,snippet",type='video',q=QUERY,videoDuration='short',videoDefinition='high',maxResults=MAXRESULTS,pageToken=response["nextPageToken"])
-            response = request.execute()
-            self.dataop.data_operation(response,self.db_op)
-    
-    
-    def startcalls(self):
-        request = self.search_videos()
-        n = datetime.datetime.now(datetime.timezone.utc)
-        n=n.isoformat()
-        while(1) :
-            print("sleeping.......................")
-            time.sleep(WAIT_TIME)
-            request = self.search_videos_since(n)
-            n = datetime.datetime.now(datetime.timezone.utc)
-            n=n.isoformat()
-
-
-            
-
+api.add_resource(SearchVideo,"/api/v1/search")
+api.add_resource(FetchVideo,"/api/v1/fetch/all")
 
 if __name__ == '__main__':
-    search_video=SearchVideosYoutube()
-    search_video.startcalls()
+    app.run()
